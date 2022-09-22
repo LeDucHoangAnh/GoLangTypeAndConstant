9a9070f78ab663e879481c83ce5ea30c360bf9c2
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-22 20:51:11 +0700

    khởi tạo go mod và in biến có kiểu dữ liệu là boolean

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-typeandconst
+
+go 1.19
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Boolean
+	var myBool bool = true //false
+	fmt.Println(myBool)
+
+	var mySecondBool bool = false
+	fmt.Println(mySecondBool)
 }
